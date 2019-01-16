23ce82e5f6b35cdb198880faecab2242831c7e14
Luke Montgomery <luke.p.montgomery@gmail.com>
2019-01-16 11:21:12 +0000

    first commit

---
+++ iteration.js
@@ -0,0 +1,33 @@
+
+function iterate() {
+    
+    let number = +document.getElementById("numberToIterate").value;
+    console.log(number);
+    while (number != 1) {
+        let br = document.createElement("br");
+        document.getElementById("list").appendChild(br);
+        if (number % 3 == 0){
+            let n = document.createTextNode(number+ " divide by 3");
+            document.getElementById("list").appendChild(br);
+            document.getElementById("list").appendChild(n);
+            document.getElementById("list").appendChild(br);
+            number = number / 3;
+        }
+        else if (number % 3 == 1){
+            let n = document.createTextNode(number + " - 1");
+            document.getElementById("list").appendChild(br);
+            document.getElementById("list").appendChild(n);
+            document.getElementById("list").appendChild(br);
+            number = number - 1;
+        }
+        else if (number % 3 == 2){
+            let n = document.createTextNode(number + " + 1");
+            document.getElementById("list").appendChild(br);
+            document.getElementById("list").appendChild(n);
+            document.getElementById("list").appendChild(br);
+            number = +number + 1;
+        }
+        let n = document.createTextNode(number);
+        
+    }
+}
